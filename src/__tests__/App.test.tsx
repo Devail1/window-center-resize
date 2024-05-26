cdc8904a0f1dcaa037ecde2bae0720d2932423a5
Liav Edry <liav.acc@gmail.com>
2024-05-27 02:55:13 +0300

    replace act import on test

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,6 +1,5 @@
 import '@testing-library/jest-dom';
-import { render, screen } from '@testing-library/react';
-import { act } from 'react';
+import { act, render, screen } from '@testing-library/react';
 import App from '@/renderer/App';
 import mockElectron from '@/__mock__/electron';
 
