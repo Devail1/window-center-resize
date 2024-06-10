d42c4f85f9d3ed06f8db747f8193a8e24d1582d1
Liav Edry <liav.acc@gmail.com>
2024-06-11 02:44:45 +0300

    working on auto-updater

--- src/main/main.ts
+++ src/main/main.ts
@@ -8,9 +8,11 @@
  * When running `npm run build` or `npm run build:main`, this file is compiled to
  * `./src/main.js` using webpack. This gives us some performance wins.
  */
-import { BrowserWindow, app, ipcMain } from 'electron';
+import { BrowserWindow, app, dialog, ipcMain } from 'electron';
 import sourceMapSupport from 'source-map-support';
 import debug from 'electron-debug';
+import { autoUpdater } from 'electron-updater';
+import log from 'electron-log';
 import { startAutoHotkeyProcess, stopAutoHotkeyProcess } from './autohotkey';
 import { createWindow } from './window';
 import {
@@ -22,6 +24,56 @@ import {
   getSettings,
 } from './settings';
 import createTrayMenu from './tray';
+
+autoUpdater.logger = log;
+log.transports.file.level = 'info';
+log.info('App starting...');
+
+autoUpdater.on('checking-for-update', () => {
+  log.info('Checking for update...');
+});
+
+autoUpdater.on('update-available', (info) => {
+  log.info('Update available.', info);
+});
+
+autoUpdater.on('update-not-available', (info) => {
+  log.info('Update not available.', info);
+});
+
+autoUpdater.on('error', (err) => {
+  log.info('Error in auto-updater. ', err);
+});
+
+autoUpdater.on('download-progress', (progressObj) => {
+  let logMessage = `Download speed: ${progressObj.bytesPerSecond}`;
+  logMessage = `${logMessage} - Downloaded ${progressObj.percent}%`;
+  logMessage = `${logMessage} (${progressObj.transferred}/${progressObj.total})`;
+  log.info(logMessage);
+});
+
+autoUpdater.on('update-downloaded', (info) => {
+  log.info('Update downloaded', info);
+  // Show a dialog to the user to confirm restart
+  dialog
+    .showMessageBox({
+      type: 'info',
+      title: 'Update Available',
+      message:
+        'A new version is available. Do you want to restart the application to apply the updates now?',
+      buttons: ['Restart', 'Later'],
+    })
+    .then((result) => {
+      const buttonIndex = result.response;
+      if (buttonIndex === 0) {
+        autoUpdater.quitAndInstall();
+      }
+    })
+    .catch((error) => {
+      console.error('Error in auto-updater. ', error);
+    });
+  autoUpdater.quitAndInstall();
+});
 
 ipcMain.on('ipc-example', async (event, arg) => {
   const msgTemplate = (pingPong: string) => `IPC test: ${pingPong}`;
@@ -55,6 +107,9 @@ app
       closeWatcher();
       stopAutoHotkeyProcess();
     });
+
+    // Check for updates and notify
+    autoUpdater.checkForUpdatesAndNotify();
   })
   .catch((error) => {
     console.error('Error in app.whenReady:', error);
